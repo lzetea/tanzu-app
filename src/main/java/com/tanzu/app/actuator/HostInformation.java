09c140ea84217446839e429784727d2c9667e476
Fabien MARTIN <fmartin@vmware.com>
2021-10-12 20:55:22 +0200

    Add OpenSSL information

---
+++ src/main/java/com/tanzu/app/actuator/HostInformation.java
@@ -0,0 +1,40 @@
+package com.tanzu.app.actuator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.actuate.info.Info;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.stereotype.Component;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+// https://docs.spring.io/spring-boot/docs/current/reference/html/production-ready-features.html#production-ready-application-info-custom
+@Component
+public class HostInformation implements InfoContributor {
+
+    private static final Logger LOG = LoggerFactory.getLogger(HostInformation.class);
+
+    @Override
+    public void contribute(Info.Builder builder) {
+
+        StringBuilder sb = new StringBuilder("openssl version : ");
+        try {
+
+            Runtime rt = Runtime.getRuntime();
+            Process pr = rt.exec("openssl version");
+
+            BufferedReader input = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+
+            String line;
+
+            while((line=input.readLine()) != null) {
+                sb.append(line);
+            }
+
+            int exitVal = pr.waitFor();
+        } catch(Exception e) {
+            LOG.warn("An error occurred while retrieving Openssl version", e);
+        }
+    }
+}
