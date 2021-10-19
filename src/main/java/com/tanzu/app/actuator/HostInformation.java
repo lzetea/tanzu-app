26635815efe10c501ff1306fb0985f119b15afb0
Fabien MARTIN <fmartin@vmware.com>
2021-10-19 13:48:20 +0200

    Add host information

--- src/main/java/com/tanzu/app/actuator/HostInformation.java
+++ src/main/java/com/tanzu/app/actuator/HostInformation.java
@@ -6,8 +6,8 @@ import org.springframework.boot.actuate.info.Info;
 import org.springframework.boot.actuate.info.InfoContributor;
 import org.springframework.stereotype.Component;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 // https://docs.spring.io/spring-boot/docs/current/reference/html/production-ready-features.html#production-ready-application-info-custom
 @Component
@@ -18,30 +18,12 @@ public class HostInformation implements InfoContributor {
     @Override
     public void contribute(Info.Builder builder) {
 
-        StringBuilder sb = new StringBuilder();
+        String host = "Unknown";
         try {
-
-            Runtime rt = Runtime.getRuntime();
-            Process pr = rt.exec("openssl version -a");
-
-            BufferedReader input = new BufferedReader(new InputStreamReader(pr.getInputStream()));
-
-            String line;
-            int i = 0;
-
-            while((line=input.readLine()) != null) {
-                // Keep only first 3 lines of output
-                if(i < 3){
-                    sb.append(line).append(" ");
-                }
-                i++;
-            }
-
-            int exitVal = pr.waitFor();
-        } catch(Exception e) {
-            LOG.warn("An error occurred while retrieving Openssl version", e);
+            host = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            LOG.warn("Cannot access to the host address", e);
         }
-
-        builder.withDetail("openssl", sb.toString());
+        builder.withDetail("host", host);
     }
 }
