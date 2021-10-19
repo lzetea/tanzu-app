265cf0d858d3c7c7a954f15600f629ca434e7271
Fabien MARTIN <fmartin@vmware.com>
2021-10-19 07:59:33 +0200

    Add gui

--- src/main/java/com/tanzu/app/controller/HelloController.java
+++ src/main/java/com/tanzu/app/controller/HelloController.java
@@ -1,24 +1,34 @@
 package com.tanzu.app.controller;
 
+import com.tanzu.app.domain.Choice;
 import com.tanzu.app.domain.Hello;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
-@RestController
+@Controller
 public class HelloController {
 
     private static final Logger LOG = LoggerFactory.getLogger(HelloController.class);
-    private static final String DEFAULT_NAME = "Stranger";
 
-    private final AtomicLong counter = new AtomicLong();
+    private final AtomicInteger counter = new AtomicInteger();
+    private final List<Hello> hellos = new ArrayList<>();
+
+    public HelloController(){
+        hellos.add(new Hello(new Choice("/images/cat.gif", "Cat"), new Choice("/images/dog.gif", "Dog")));
+        hellos.add(new Hello(new Choice("/images/ski.gif", "Ski"), new Choice("/images/snowboard.gif", "Snowboard")));
+        hellos.add(new Hello(new Choice("/images/beach.gif", "Beach"), new Choice("/images/mountain.gif", "Mountain")));
+    }
 
     @GetMapping("/")
-    public Hello hello(@RequestParam(name = "name", required = false, defaultValue = DEFAULT_NAME) String name) {
-        return new Hello(counter.incrementAndGet(), String.format("Hello, %s!", name));
+    public String greeting(Model model) {
+        model.addAttribute("hello", hellos.get(counter.getAndIncrement() % hellos.size()));
+        return "hello";
     }
 }
