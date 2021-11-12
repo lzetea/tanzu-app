200b5643badb54a2c2ee7bfcca1a97a138196e97
Fabien MARTIN <fmartin@vmware.com>
2021-11-12 14:18:55 +0100

    Add Kubernetes secrets

--- src/main/java/com/tanzu/app/controller/GuiController.java
+++ src/main/java/com/tanzu/app/controller/GuiController.java
@@ -3,8 +3,6 @@ package com.tanzu.app.controller;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,21 +11,18 @@ import com.tanzu.app.domain.Hello;
 import com.tanzu.app.repository.HelloRepository;
 
 @Controller
-public class HelloController {
-
-	private static final Logger LOG = LoggerFactory.getLogger(HelloController.class);
+public class GuiController {
 
 	private HelloRepository helloRepository;
 
 	private final AtomicInteger counter = new AtomicInteger();
 
-	public HelloController(HelloRepository helloRepository) {
+	public GuiController(HelloRepository helloRepository) {
 		this.helloRepository = helloRepository;
 	}
 
 	@GetMapping("/")
 	public String greeting(Model model) {
-		LOG.debug("Looking for hellos");
 		List<Hello> hellos = helloRepository.findAllEagered();
 		model.addAttribute("hello", hellos.get(counter.getAndIncrement() % hellos.size()));
 		return "hello";
